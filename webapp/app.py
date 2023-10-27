efa4993c7a5671a3a1c70f2bca926c2f52b060f3
Gabor Major <61381495+NotYetTerminal@users.noreply.github.com>
2023-10-27 20:09:42 +0100

    Update

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template  # from module import Class.
+from flask import Flask, render_template, request  # from module import Class.
 
+
+import os 
 
 import hfpy_utils
 import swim_utils
@@ -24,7 +26,7 @@ def display_chart():
         the_times,
         converts,
         the_average,
-    ) = swim_utils.get_swimmers_data("Katie-9-100m-Free.txt")
+    ) = swim_utils.get_swimmers_data("Darius-13-100m-Fly.txt")
 
     the_title = f"{name} (Under {age}) {distance} {stroke}"
     from_max = max(converts) + 50
@@ -38,6 +40,25 @@ def display_chart():
         average=the_average,
         data=the_data,
     )
+
+
+@app.get("/getswimmers")
+def get_swimmers_names():
+    files = os.listdir(swim_utils.FOLDER)
+    files.remove(".DS_Store")
+    names = set()
+    for swimmer in files:
+        names.add(swim_utils.get_swimmers_data(swimmer)[0])
+    return render_template(
+        "select.html",
+        title="Select a swimmer to chart",
+        data=sorted(names),
+    )
+
+
+@app.post("/displayevents")
+def get_swimmer_events():
+    return request.form["swimmer"]
 
 
 if __name__ == "__main__":
